5ab6cbaa6b8419dca2fa94064e1c179711c60ebe
Abongile <njkabo001@myuct.ac.za>
2025-10-02 23:32:31 +0200

    Task 3: TIM2_Ticks

--- Practical4/Practical_4/Core/Src/main.c
+++ Practical4/Practical_4/Core/Src/main.c
@@ -116,7 +116,7 @@ uint32_t Drum_LUT[NS] = {
 
 
 // TODO: Equation to calculate TIM2_Ticks
-uint32_t TIM2_Ticks = 0; // How often to write new LUT value
+uint32_t TIM2_Ticks = 125; // How often to write new LUT value -- TIM2CLK/NS*FSIGNAL
 uint32_t DestAddress = (uint32_t) &(TIM3->CCR3); // Write LUT TO TIM3->CCR3 to modify PWM duty cycle
 
 
